b7bbd02da2e51f4b204af6a5942e74e7774d3d6b
MikkyMeow <arturakk2015@gmail.com>
2021-03-14 01:33:10 +0300

    added styles for FollowItem component

--- src/components/elements/FollowItem.tsx
+++ src/components/elements/FollowItem.tsx
@@ -2,11 +2,19 @@ import { Box, Button, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   imageBox: {
     width: 48,
     height: 48,
     marginRight: 8,
+  },
+  image: {
+    borderRadius: '50%',
   },
   userInfo: {
     marginRight: 'auto',
@@ -17,16 +25,17 @@ const FollowItem = () => {
   const classes = useStyles();
 
   return (
-    <Box display='flex' width='100%' justifyContent='space-between'>
+    <Box className={classes.root}>
       <Box className={classes.imageBox}>
         <img
-          src='https://media.istockphoto.com/illustrations/user-profile-icon-glassy-vibrant-sky-blue-round-button-illustration-illustration-id1164425002?k=6&m=1164425002&s=170667a&w=0&h=3M6yc6H4EHXRWzDOwBRsd653WME4ohXF4CwOxs8hRn4='
+          src='https://abs.twimg.com/sticky/default_profile_images/default_profile_x96.png'
           alt='user'
+          className={classes.image}
         />
       </Box>
       <Box className={classes.userInfo}>
-        <Typography>Pavel Durov</Typography>
-        <Typography>@durov</Typography>
+        <Typography gutterBottom>Pavel Durov</Typography>
+        <Typography gutterBottom>@durov</Typography>
       </Box>
       <Button variant='outlined' color='primary'>
         Follow
